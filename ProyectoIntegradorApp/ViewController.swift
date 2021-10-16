7d814b47b693bf1589e2c56b61d4155e56a3ba06
Arturo Melendez <arturo.pemju@outlook.com>
2021-10-16 16:59:48 -0500

    Initial Commit

---
+++ ProyectoIntegradorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoIntegradorApp
+//
+//  Created by Claudia Meléndez on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
